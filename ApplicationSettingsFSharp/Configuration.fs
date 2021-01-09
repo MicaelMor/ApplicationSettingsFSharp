8455cd24bfe57dc7fee00e3bc6aa367da896acf9
Micael Morais <micaelmor@gmail.com>
2021-01-09 17:42:23 +0000

    Release 1.0.0.0

---
+++ ApplicationSettingsFSharp/Configuration.fs
@@ -0,0 +1,20 @@
+﻿module ApplicationSettingsFSharp.Configuration
+
+open ApplicationSettingsFSharp.Helper
+open Config.Net
+
+let Settings<'T when 'T: not struct> originConfigFileNameWithoutExtension destinationPath destinationConfigFileNameWithoutExtension =
+
+    copySettingsFile originConfigFileNameWithoutExtension destinationConfigFileNameWithoutExtension false destinationPath
+
+    let fullConfigPath =
+        System.IO.Path.Combine [| destinationPath
+                                  (destinationConfigFileNameWithoutExtension + ".json") |]
+
+    let settings =
+        ConfigurationBuilder<'T>()
+        |> fun x ->
+            x.UseJsonFile fullConfigPath
+            |> fun x -> x.Build()
+
+    settings
---
+++ ApplicationSettingsFSharp/Helper.fs
@@ -0,0 +1,24 @@
+﻿module ApplicationSettingsFSharp.Helper
+
+open System
+open System.IO
+
+let copySettingsFile originConfigFileNameWithoutExtension destinationConfigFileNameWithoutExtension shouldReplaceExistingConfig destinationPath =
+    let assemblyName =
+        Reflection.Assembly.GetExecutingAssembly().GetName().Name
+
+    let originalFilePath =
+        Reflection.Assembly.GetExecutingAssembly().Location.Replace((assemblyName + ".dll"), (originConfigFileNameWithoutExtension + ".json"))
+
+    let destinationFilePath =
+        Path.Combine [| destinationPath
+                        destinationConfigFileNameWithoutExtension + ".json" |]
+
+    (FileInfo(destinationFilePath)).Directory.Create()
+
+    if not
+       <| File.Exists destinationFilePath
+       && not shouldReplaceExistingConfig then
+        File.Copy(originalFilePath, destinationFilePath)
+    else if shouldReplaceExistingConfig then
+        File.Copy(originalFilePath, destinationFilePath, shouldReplaceExistingConfig)
